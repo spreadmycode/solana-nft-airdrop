75a58e50f2298dfff8b73f431d105a165e6460d6
spreadmycode <91585048+spreadmycode@users.noreply.github.com>
2022-06-19 15:37:47 -0800

    engine prepared

--- airdrop.js
+++ airdrop.js
@@ -1,39 +1,49 @@
-const fs = require('fs');
+const fs = require("fs");
 const readline = require("readline");
-const shelljs = require('shelljs');
+const shelljs = require("shelljs");
 
 async function getReadLine(message) {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  return new Promise((resolve) => {
+    rl.question(message, function (value) {
+      rl.close();
+      resolve(value);
     });
-    return new Promise((resolve) => {
-      rl.question(message, function (value) {
-        rl.close();
-        resolve(value);
-      });
-    })
+  });
 }
 
 async function airdropNFT() {
-  const mintListFilePath = await getReadLine("Type mint list file path(json): ");
+  const mintListFilePath = await getReadLine(
+    "Type mint list file path(json): "
+  );
   let rawdataMint = fs.readFileSync(mintListFilePath);
   let mintList = JSON.parse(rawdataMint);
 
-  const holderListFilePath = await getReadLine("Type NFT holder list file path(json): ");
+  const holderListFilePath = await getReadLine(
+    "Type NFT holder list file path(json): "
+  );
   let rawdataHolder = fs.readFileSync(holderListFilePath);
   let holderList = JSON.parse(rawdataHolder);
 
   for (let i = 0; i < mintList.length; i++) {
     let mintAddress = mintList[i];
-    if (!mintAddress || mintAddress == undefined || mintAddress == '') continue;
+    let holderAddress = holderList[i].owner_wallet;
+    if (
+      !mintAddress ||
+      mintAddress == undefined ||
+      mintAddress == "" ||
+      !holderAddress ||
+      holderAddress == undefined ||
+      holderAddress == ""
+    )
+      continue;
 
-    let holderAddress = holderList[i];
-    if (!holderAddress || holderAddress == undefined || holderAddress == '') {
-        holderAddress = "66Ri5mnZkZaDY6uocHy4p2sYZ5PQ5MhByxiEj7jHAQM";
-    }
-
-    shelljs.exec(`spl-token transfer --allow-unfunded-recipient --fund-recipient ${mintAddress} 1 ${holderAddress}`);
+    shelljs.exec(
+      `spl-token transfer --allow-unfunded-recipient --fund-recipient ${mintAddress} 1 ${holderAddress}`
+    );
   }
 }
 
